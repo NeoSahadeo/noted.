da6abf4581484d234351de60865d47469941567e
NeoSahadeo <neosahadeo@protonmail.com>
2024-07-27 07:31:55 +0100

    :ambulance: updated svelte confg

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: {
+			handleHttpError: () => {}
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
